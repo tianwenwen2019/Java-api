e73b4fb0e7a201e4bf95e94a7814bf1084649ec4
tianwenwen2019 <1701448182>
2019-11-24 16:27:05 +0800

    提交

--- javaapiparent/java-api-object/src/main/java/com/tedu/cu/Hellow.java
+++ javaapiparent/java-api-object/src/main/java/com/tedu/cu/Hellow.java
@@ -3,5 +3,8 @@ package com.tedu.cu;
 public class Hellow {
     public static void main(String[] args) {
         System.out.println("--------");
+        System.out.println("hjxdkjd");
+        System.out.println("dghjfgsdf");
+        System.out.println("*********");
     }
 }
